71f234366d61e2191b430d19844aef4bbe8c4a72
Jerome Kelleher <jk@well.ox.ac.uk>
2021-07-27 11:17:34 +0100

    Update

--- tree_performance/c_implementation.c
+++ tree_performance/c_implementation.c
@@ -9,75 +9,75 @@
         errx(EXIT_FAILURE, "line %d: %s", __LINE__, tsk_strerror(val));                 \
     }
 
-static void
-_traverse(tsk_tree_t *tree, tsk_id_t u, int depth)
-{
-    tsk_id_t v;
-    int j;
+/* static void */
+/* _traverse(tsk_tree_t *tree, tsk_id_t u, int depth) */
+/* { */
+/*     tsk_id_t v; */
+/*     int j; */
 
-    for (j = 0; j < depth; j++) {
-        printf("    ");
-    }
-    printf("Visit recursive %lld\n", (long long) u);
-    for (v = tree->left_child[u]; v != TSK_NULL; v = tree->right_sib[v]) {
-        _traverse(tree, v, depth + 1);
-    }
-}
+/*     for (j = 0; j < depth; j++) { */
+/*         printf("    "); */
+/*     } */
+/*     printf("Visit recursive %lld\n", (long long) u); */
+/*     for (v = tree->left_child[u]; v != TSK_NULL; v = tree->right_sib[v]) { */
+/*         _traverse(tree, v, depth + 1); */
+/*     } */
+/* } */
 
-static void
-traverse_recursive(tsk_tree_t *tree)
-{
-    tsk_id_t root;
+/* static void */
+/* traverse_recursive(tsk_tree_t *tree) */
+/* { */
+/*     tsk_id_t root; */
 
-    for (root = tree->left_root; root != TSK_NULL; root = tree->right_sib[root]) {
-        _traverse(tree, root, 0);
-    }
-}
+/*     for (root = tree->left_root; root != TSK_NULL; root = tree->right_sib[root]) { */
+/*         _traverse(tree, root, 0); */
+/*     } */
+/* } */
 
-static void
-traverse_stack(tsk_tree_t *tree)
-{
-    int stack_top;
-    tsk_id_t u, v, root;
-    tsk_id_t *stack
-        = malloc(tsk_treeseq_get_num_nodes(tree->tree_sequence) * sizeof(*stack));
+/* static void */
+/* traverse_stack(tsk_tree_t *tree) */
+/* { */
+/*     int stack_top; */
+/*     tsk_id_t u, v, root; */
+/*     tsk_id_t *stack */
+/*         = malloc(tsk_treeseq_get_num_nodes(tree->tree_sequence) * sizeof(*stack)); */
 
-    if (stack == NULL) {
-        errx(EXIT_FAILURE, "Out of memory");
-    }
-    for (root = tree->left_root; root != TSK_NULL; root = tree->right_sib[root]) {
-        stack_top = 0;
-        stack[stack_top] = root;
-        while (stack_top >= 0) {
-            u = stack[stack_top];
-            stack_top--;
-            printf("Visit stack %lld\n", (long long) u);
-            /* Put nodes on the stack right-to-left, so we visit in left-to-right */
-            for (v = tree->right_child[u]; v != TSK_NULL; v = tree->left_sib[v]) {
-                stack_top++;
-                stack[stack_top] = v;
-            }
-        }
-    }
-    free(stack);
-}
+/*     if (stack == NULL) { */
+/*         errx(EXIT_FAILURE, "Out of memory"); */
+/*     } */
+/*     for (root = tree->left_root; root != TSK_NULL; root = tree->right_sib[root]) { */
+/*         stack_top = 0; */
+/*         stack[stack_top] = root; */
+/*         while (stack_top >= 0) { */
+/*             u = stack[stack_top]; */
+/*             stack_top--; */
+/*             printf("Visit stack %lld\n", (long long) u); */
+/*             /1* Put nodes on the stack right-to-left, so we visit in left-to-right *1/ */
+/*             for (v = tree->right_child[u]; v != TSK_NULL; v = tree->left_sib[v]) { */
+/*                 stack_top++; */
+/*                 stack[stack_top] = v; */
+/*             } */
+/*         } */
+/*     } */
+/*     free(stack); */
+/* } */
 
-static void
-traverse_upwards(tsk_tree_t *tree)
-{
-    const tsk_id_t *samples = tsk_treeseq_get_samples(tree->tree_sequence);
-    tsk_size_t num_samples = tsk_treeseq_get_num_samples(tree->tree_sequence);
-    tsk_size_t j;
-    tsk_id_t u;
+/* static void */
+/* traverse_upwards(tsk_tree_t *tree) */
+/* { */
+/*     const tsk_id_t *samples = tsk_treeseq_get_samples(tree->tree_sequence); */
+/*     tsk_size_t num_samples = tsk_treeseq_get_num_samples(tree->tree_sequence); */
+/*     tsk_size_t j; */
+/*     tsk_id_t u; */
 
-    for (j = 0; j < num_samples; j++) {
-        u = samples[j];
-        while (u != TSK_NULL) {
-            printf("Visit upwards: %lld\n", (long long) u);
-            u = tree->parent[u];
-        }
-    }
-}
+/*     for (j = 0; j < num_samples; j++) { */
+/*         u = samples[j]; */
+/*         while (u != TSK_NULL) { */
+/*             printf("Visit upwards: %lld\n", (long long) u); */
+/*             u = tree->parent[u]; */
+/*         } */
+/*     } */
+/* } */
 
 int
 main(int argc, char **argv)
@@ -85,6 +85,8 @@ main(int argc, char **argv)
     int ret;
     tsk_treeseq_t ts;
     tsk_tree_t tree;
+    tsk_vargen_t vargen;
+    tsk_variant_t *var;
 
     if (argc != 2) {
         errx(EXIT_FAILURE, "usage: <tree sequence file>");
@@ -96,12 +98,22 @@ main(int argc, char **argv)
     ret = tsk_tree_first(&tree);
     check_tsk_error(ret);
 
-    traverse_recursive(&tree);
+    ret = tsk_vargen_init(&vargen, &ts, NULL, 0, NULL, 0);
+    check_tsk_error(ret);
 
-    traverse_stack(&tree);
+    while ((ret = tsk_vargen_next(&vargen, &var)) == 1) {
+        printf("variant %d\n", var->site->id);
 
-    traverse_upwards(&tree);
+    }
+    check_tsk_error(ret);
 
+/*     traverse_recursive(&tree); */
+
+/*     traverse_stack(&tree); */
+
+/*     traverse_upwards(&tree); */
+
+    tsk_vargen_free(&vargen);
     tsk_tree_free(&tree);
     tsk_treeseq_free(&ts);
     return 0;
