8ceab2ad608fa0eb0d7d63916abd9dc48429816f
molpopogen <krthornt@uci.edu>
2021-08-12 13:41:18 -0700

    I can copy trees now

--- tree_performance/cpp_implementation_stack_allocated.cc
+++ tree_performance/cpp_implementation_stack_allocated.cc
@@ -1,6 +1,8 @@
 #include <tskit.h>
 
 #include <vector>
+#include <utility>
+#include <iostream>
 #include <memory>
 
 struct TskTreeSeq
@@ -48,11 +50,16 @@ struct TskTree
 
 struct Node
 {
+    int id;
     std::vector<Node> children;
+
+    Node() : id{-1}, children{}
+    {
+    }
 };
 
 static void
-_traverse(tsk_tree_t *tree, tsk_id_t u, tsk_id_t parent, int depth)
+_traverse(const tsk_tree_t *tree, tsk_id_t u, tsk_id_t parent, int depth)
 {
     tsk_id_t v;
     int j;
@@ -69,7 +76,7 @@ _traverse(tsk_tree_t *tree, tsk_id_t u, tsk_id_t parent, int depth)
 }
 
 static void
-traverse_recursive(tsk_tree_t *tree)
+traverse_recursive(const tsk_tree_t *tree)
 {
     tsk_id_t root;
 
@@ -77,6 +84,82 @@ traverse_recursive(tsk_tree_t *tree)
         {
             _traverse(tree, root, -1, 0);
         }
+}
+
+int
+count_roots(const tsk_tree_t *tree)
+{
+    int nroots = 0;
+    for (auto root = tree->left_root; root != TSK_NULL; root = tree->right_sib[root])
+        {
+            nroots += 1;
+        }
+    return nroots;
+}
+
+void
+build_node_stack_recursive(const tsk_tree_t *tree, tsk_id_t u, tsk_id_t parent,
+                           std::vector<std::pair<int, Node>> &node_stack)
+{
+    node_stack[u].first = parent;
+    node_stack[u].second.id = u;
+    for (auto c = tree->left_child[u]; c != TSK_NULL; c = tree->right_sib[c])
+        {
+            build_node_stack_recursive(tree, c, u, node_stack);
+        }
+}
+
+void
+build_recursive(const tsk_tree_t *tree, std::vector<std::pair<int, Node>> &node_stack)
+{
+    node_stack.resize(tree->left_root + 1);
+    build_node_stack_recursive(tree, tree->left_root, -1, node_stack);
+}
+
+static Node
+build_tree(tsk_tree_t *tree)
+{
+    std::vector<std::pair<int, Node>> node_stack;
+
+    build_recursive(tree, node_stack);
+
+    if (node_stack.empty())
+        {
+            throw std::runtime_error("node stack is empty");
+        }
+
+    for (std::size_t i = 0; i < node_stack.size(); ++i)
+        {
+            if (node_stack[i].first != TSK_NULL)
+                {
+                    // node is not a root node
+                    node_stack[node_stack[i].first].second.children.push_back(
+                        node_stack[i].second);
+                }
+        }
+
+    Node root{node_stack.back().second};
+    return root;
+}
+
+void
+_traverse_confirm(const Node &node, int parent, int depth)
+{
+    for (int d = 0; d < depth; ++d)
+        {
+            std::cout << "    ";
+        }
+    std::cout << "Visit confirm " << node.id << ' ' << parent << ' ' << depth << '\n';
+    for (auto &c : node.children)
+        {
+            _traverse_confirm(c, node.id, depth + 1);
+        }
+}
+
+void
+traverse_recursive_confirm(const Node &node)
+{
+    _traverse_confirm(node, -1, 0);
 }
 
 static TskTreeSeq
@@ -90,5 +173,12 @@ main(int argc, char **argv)
 {
     auto ts = load_treeseq(argv[1]);
     TskTree tree(ts);
+    if (count_roots(&tree.tree) != 1)
+        {
+            throw std::invalid_argument("tree must have a single root");
+        }
     traverse_recursive(&tree.tree);
+
+    auto cpp_tree = build_tree(&tree.tree);
+    traverse_recursive_confirm(cpp_tree);
 }
